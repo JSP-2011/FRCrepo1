e10743a4e9cf8ecb56ab9f1b3cf8fb12b7e98be4
JSP <jobanpreet.s.pannu@gmail.com>
2026-01-12 19:01:21 -0600

    Initial commit

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "SteelTalonsLibrary";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 47;
+  public static final String GIT_SHA = "3ca836514e8861be52ebf01116efa2204e102617";
+  public static final String GIT_DATE = "2026-01-11 15:40:47 CST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2026-01-11 22:54:44 CST";
+  public static final long BUILD_UNIX_TIME = 1768193684250L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants() {}
+}
